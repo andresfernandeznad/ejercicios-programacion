30bec314899845ee182892ad375d61b77590f2b5
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-11 22:21:36 +0200

    Nuevo ejercicio del tema 4

---
+++ Ejercicios/SentenciaCondicional/Ejercicio06.java
@@ -0,0 +1,19 @@
+//Ejercicio04 de Sentencia Condicional
+
+//@author Andrés Fernández Nadales
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.print("Introduzca el valor de la altura: ");
+    double altura = s.nextDouble();
+    double tiempo = Math.sqrt ((2*altura)/9.81);;
+    if (altura < 0) {
+      System.out.print("Altura no valida.");
+    } else {
+      System.out.print("Su objeto tardará en caer: " + tiempo + " segundos.");
+    }
+  }
+}
