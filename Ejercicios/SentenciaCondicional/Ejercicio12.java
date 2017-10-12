be40909524b28a18a23e2b2e2d665e4ab2f16015
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-12 20:24:02 +0200

    Nuevo ejercicio del tema 4

--- Ejercicios/SentenciaCondicional/Ejercicio12.java
+++ Ejercicios/SentenciaCondicional/Ejercicio12.java
@@ -11,9 +11,55 @@ public class Ejercicio12 {
     int contador = 0;
     System.out.print("¿Java es un lenguaje de programación? ");
     String respuesta = s.next().toLowerCase();
-    if (respuesta == "si" ) {
-      System.out.print("Contador: " + contador);
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
     }
+    System.out.print("¿Java es más antiguo que C? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("no") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Hacemos los estilos con HTML? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("no") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Linux es gratis? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Windows es de pago? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿La memoria ram está hecha del mismo material que la GPU? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("no") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Que una base de datos sea integra implica que no tenga errores? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Usamos MySQL en BBDD? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Unix es predecesor de Linux? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
+    }
+    System.out.print("¿Para crear un directorio escribimos mkdir? ");
+    respuesta = s.next().toLowerCase();
+    if (respuesta.equals("si") ) {
+      contador = contador + 1;
+    }
+    System.out.print("Has sacado un: " + contador);
   }
 }
     
