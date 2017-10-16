99c33207e13295d21924b6d9b8f9b0a28c6cfd2c
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-16 09:12:54 +0200

    Actualizado ejercicio 22

--- Ejercicios/SentenciaCondicional/Ejercicio22.java
+++ Ejercicios/SentenciaCondicional/Ejercicio22.java
@@ -12,21 +12,32 @@ public class Ejercicio22 {
     System.out.print("Introduce la hora de ese día y minutos: ");
     int hora = s.nextInt();
     int minuto = s.nextInt();
-    if (dia.equals("lunes")) {
-      int horasTotal = (4 * 24 * hora * 60);
-      System.out.print("Quedan: " + (horasTotal + minuto) + "minutos");
-    } else if (dia.equals("martes")) {
-      int horasTotal = (4 * 24 * hora * 60);
-      System.out.print("Quedan: " + (horasTotal + minuto) + "minutos");
-    } else if (dia.equals("miercoles")) {
-      int horasTotal = (3 * 24 * hora * 60);
-      System.out.print("Quedan: " + (horasTotal + minuto) + "minutos");
-    } else if (dia.equals("jueves")) {
-      int horasTotal = (2 * 24 * hora * 60);
-      System.out.print("Quedan: " + (horasTotal + minuto) + "minutos");
+    if (((dia.equals("viernes"))&&(hora >= 15))) {
+      System.out.print("Ya estás en el fin de semana.");
     } else if (dia.equals("viernes")) {
-      int horasTotal = (1 * 24 * hora * 60);
-      System.out.print("Quedan: " + (horasTotal + minuto) + "minutos");
+      int horasTotal = (hora * 60);
+      System.out.print("Quedan: " + (horasTotal + minuto + (horasTotal + minuto) - 15*60) + " minutos");
     }
+    if (dia.equals("lunes")) {
+      int horasTotal = ((4 * 24 + hora) * 60);
+      int minutosTres = (((4 * 24) + 15) * 60);
+      System.out.println("Quedan: " + (horasTotal + minuto + 
+      (((horasTotal + minuto) - minutosTres))) + " minutos");
+    } else if (dia.equals("martes")) {
+      int horasTotal = ((3 * 24 + hora) * 60);
+      int minutosTres = (((3 * 24) + 15) * 60);
+      System.out.println("Quedan: " + (horasTotal + minuto + 
+      (((horasTotal + minuto) - minutosTres))) + " minutos");
+    } else if (dia.equals("miercoles")) {
+      int horasTotal = ((2 * 24 + hora) * 60);
+      int minutosTres = (((2 * 24) + 15) * 60);
+      System.out.println("Quedan: " + (horasTotal + minuto + 
+      (((horasTotal + minuto) - minutosTres))) + " minutos");
+    } else if (dia.equals("jueves")) {
+      int horasTotal = ((24 + hora) * 60);
+      int minutosTres = ((24 + 15) * 60);
+      System.out.println("Quedan: " + (horasTotal + minuto + 
+      (((horasTotal + minuto) - minutosTres))) + " minutos");
+    } 
   }
 }
