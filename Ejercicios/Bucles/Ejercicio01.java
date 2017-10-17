1d826ce176c3fd9006073fb6abf3a293d8dcf793
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-17 14:31:53 +0200

    Añadido tema bucles y ejercicio

---
+++ Ejercicios/Bucles/Ejercicio01.java
@@ -0,0 +1,14 @@
+//Ejercicio01 de Bucles
+
+//@author Andrés Fernández Nadales
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    for (int i = 0; i <=100; ++i) {
+      System.out.print(" " + i*5 + " ");
+    }
+  }
+}
