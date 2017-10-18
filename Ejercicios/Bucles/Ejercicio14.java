cc205e6952f2ee7cd487e30447a23e595ba4ad90
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-18 10:18:00 +0200

    Añadido ejercicio nuevo

---
+++ Ejercicios/Bucles/Ejercicio14.java
@@ -0,0 +1,20 @@
+//Ejercicio14 de Bucles
+
+//@author Andrés Fernández Nadales
+
+import java.util.Scanner;
+
+public class Ejercicio14 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.print("Introduce el número base: ");
+    int base = s.nextInt();
+    System.out.print("Introduce el exponente(número entero y positivo): ");
+    int exponente = s.nextInt();
+    int potencia = 1;
+    for (int i = 1; i <= exponente; i++){
+      potencia = potencia * base;
+    }
+    System.out.print("Su potencia es: " + potencia);
+  }
+}
