bc860040613c9498a865c565911e90cb54abd922
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-23 20:52:22 +0200

    Añadido nuevo ejercicio

---
+++ Ejercicios/Bucles/Ejercicio31.java
@@ -0,0 +1,25 @@
+/**Ejercicio31 de Bucles
+*
+*@author Andrés Fernández Nadales
+* 
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio31 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.print("Introduce la altura de la L: ");
+    int altura = s.nextInt();
+    int horizontal = ((altura/2) + 1);
+    int i = 0;
+    while (i < altura - 1) {
+      System.out.println("*");
+      ++i;
+    }
+    while (horizontal > 0) {
+      System.out.print("*");
+      --horizontal;
+    }
+  }
+}
