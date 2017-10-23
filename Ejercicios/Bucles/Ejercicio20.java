b2cec846569da5b0c8a4c9fbadbf2255ba07f20e
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-23 10:58:44 +0200

    Añadido nuevos ejercicios

--- Ejercicios/Bucles/Ejercicio20.java
+++ Ejercicios/Bucles/Ejercicio20.java
@@ -1,10 +1,12 @@
-//Ejercicio19 de Bucles
-
-//@author Andrés Fernández Nadales
+/**Ejercicio20 de Bucles
+*
+*@author Andrés Fernández Nadales
+* 
+*/
 
 import java.util.Scanner;
 
-public class Ejercicio19 {
+public class Ejercicio20 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
     System.out.print("Introduce el carácter con el que hará la pirámide: ");
@@ -19,7 +21,11 @@ public class Ejercicio19 {
         System.out.print(" ");
       }
       for (int x = 1; x <= ancho; ++x) {
-        System.out.print(caracter);
+        if ((x == 1)||(x == ancho)||(ancho == (alturaFinal*2)-1)){
+          System.out.print(caracter);
+        } else {
+          System.out.print(" ");
+        }
       }
       --espacios;
       ancho += 2;
