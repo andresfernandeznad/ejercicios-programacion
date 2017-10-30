f53393436792df1549f74d9bbb0e6ab63c33554b
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-10-30 11:02:17 +0100

    Añadido nuevo ejercicio

--- Ejercicios/Bucles/Ejercicio40.java
+++ Ejercicios/Bucles/Ejercicio40.java
@@ -7,28 +7,59 @@
 import java.util.Scanner;
 
 public class Ejercicio40 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.print("Introduzca la altura: ");
-    int alturaIntroducida = s.nextInt();
-    int altura = 1;
-    int espacios = alturaIntroducida - 2;
-    while ((alturaIntroducida < 3)||((alturaIntroducida%2)==0)) {
-      System.out.println("Introduzca la altura: ");
-      alturaIntroducida = s.nextInt();
+    int altura = s.nextInt();
+    
+    //Controlador de datos
+    while ((altura < 3)||((altura %2 )==0)) {
+      System.out.print("Datos erróneos, introduzca de nuevo la altura: ");
+      altura = s.nextInt();
       int x = 0;
     }
-    for (int i = 0; i < alturaIntroducida - 2; ++i) {
-      System.out.println();
-      for (int j = 0; j < espacios; ++j) {
+    
+    //Pintar parte superior
+    int espacioInterior = -1;
+    int espacioExterior = altura / 2;
+    int alturaSuperior = (altura / 2) + 1;
+    for (int i = 0; i < alturaSuperior; ++i) {
+      for (int j = 0; j < espacioExterior; ++j) {
         System.out.print(" ");
       }
-      for (int x = 0; x < altura; ++x) {
+      System.out.print("*");
+      
+      //Mostrar espacios interiores
+      if (i > 0) {
+        for (int x = 0; x < espacioInterior; ++x) {
+          System.out.print(" ");
+        }
         System.out.print("*");
       }
-    --espacios;
-    ++altura;
+      System.out.println();
+      --espacioExterior;
+      espacioInterior += 2;
+    }
+    
+    //Pintar parte inferior
+    int alturaInferior = altura / 2;
+    espacioExterior = 1;
+    espacioInterior -= 4;
+    
+    for (int i = 0; i < alturaInferior; ++i) {
+      for (int j = 0; j < espacioExterior; ++j) {
+        System.out.print(" ");
+      }
+      System.out.print("*");
+      for (int x = 0; x < espacioInterior; ++x) {
+        System.out.print(" ");
+      }
+      if (i != alturaInferior - 1) {
+        System.out.print("*");
+      }
+      System.out.println();
+      espacioExterior++;
+      espacioInterior -= 2;
     }
   }
 }
-      
--- Ejercicios/Bucles/Ejercicio46.java
+++ Ejercicios/Bucles/Ejercicio46.java
@@ -9,3 +9,41 @@ import java.util.Scanner;
 public class Ejercicio46 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
+    System.out.print("Introduce la altura del rectángulo: ");
+    int altura = s.nextInt();
+    System.out.print("Introduce la anchura del rectángulo: ");
+    int anchura = s.nextInt();
+    while ((anchura < 2)||(altura < 2)) {
+      System.out.println("Datos erróneos.");
+      System.out.print("Introduce la altura del rectángulo: ");
+      altura = s.nextInt();
+      System.out.print("Introduce la anchura del rectángulo: ");
+      anchura = s.nextInt();
+    }
+    
+    //Pintar primera línea
+    
+    for (int i = 0; i < anchura; ++i) {
+      System.out.print("*");
+    }
+    System.out.println();
+    
+    //Pintar parte central del rectángulo
+    
+    int alturaCentral = altura - 2;
+    int espacios = anchura - 2;
+    for (int i = 0; i < alturaCentral; ++i) {
+      System.out.print("*");
+      for (int j = 0; j < espacios; ++j) {
+        System.out.print(" ");
+      }
+      System.out.println("*");
+    }
+    
+    //Pintar última línea
+    
+    for (int i = 0; i < anchura; ++i) {
+      System.out.print("*");
+    }
+  }
+}
