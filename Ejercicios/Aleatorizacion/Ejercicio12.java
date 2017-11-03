3834a6df161b35300aafb3fed1d579b3a91c4965
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-11-03 20:33:49 +0100

    Añadido nuevo ejercicio

--- Ejercicios/Aleatorizacion/Ejercicio12.java
+++ Ejercicios/Aleatorizacion/Ejercicio12.java
@@ -9,3 +9,11 @@ import java.util.Scanner;
 public class Ejercicio12 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
+    char valor;
+    String verde = "\033[32m";
+    for (int i = 1; i > 0; ++i) {
+      valor = (char)((int)((Math.random() * 95) + 32));
+      System.out.print(verde + valor);
+    }
+  }
+}
