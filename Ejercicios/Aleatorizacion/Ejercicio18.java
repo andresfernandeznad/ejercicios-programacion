f39c7ff1acd4586ba995903f0be3ad7bcc2090c6
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-11-14 14:27:29 +0100

    Añadido nuevo ejercicio

---
+++ Ejercicios/Aleatorizacion/Ejercicio18.java
@@ -0,0 +1,43 @@
+/**Ejercicio18 de Aleatorización
+*
+*@author Andrés Fernández Nadales
+* 
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int color = (int)(Math.random()*6);
+    int aux = color;
+    int i = 0;
+    while ((aux == color) && (i < 3)) {
+      color = (int)(Math.random()*6);
+      if (aux !=  color) {
+        switch (color) {
+          case 0:
+            System.out.print("rojo ");
+            break;
+          case 1:
+            System.out.print("azul ");
+            break;
+          case 2:
+            System.out.print("verde ");
+            break;
+          case 3:
+            System.out.print("amarillo ");
+            break;
+          case 4:
+            System.out.print("naranja ");
+            break;
+          case 5:
+            System.out.print("violeta ");
+            break;
+        }
+      }
+      aux = color;
+      ++i;
+    }
+  }
+}
