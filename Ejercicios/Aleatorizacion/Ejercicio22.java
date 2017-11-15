68c86a75feb8213981cf3772d43b6678bf94e432
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-11-15 09:41:26 +0100

    Añadido nuevo ejercicio

--- Ejercicios/Aleatorizacion/Ejercicio22.java
+++ Ejercicios/Aleatorizacion/Ejercicio22.java
@@ -9,7 +9,18 @@ import java.util.Scanner;
 public class Ejercicio22 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
+    int espacios = 13;
+    int posicion = (int)(Math.random() * 3) - 1;
+    System.out.print("Introduzca la longitud de la serpiente: ");
+    int longitud = s.nextInt();
     System.out.println("            @");
-    
+    for (int i = 1; i < longitud; i++) {
+      posicion = (int)(Math.random() * 3) - 1;
+      posicion = posicion + 12;
+      for (int j = 0; j < posicion; ++j) {
+        System.out.print(" ");
+      }
+      System.out.println("*");
+    }
   }
 }
