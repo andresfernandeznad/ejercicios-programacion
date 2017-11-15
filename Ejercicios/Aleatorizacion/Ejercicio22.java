685773c790a814d1028ddb8cca21b89bcc1e2e8c
Andrés Fernández Nadales <andresfernandeznadales@gmail.com>
2017-11-15 09:31:56 +0100

    Añadido nuevo ejercicio

--- Ejercicios/Aleatorizacion/Ejercicio22.java
+++ Ejercicios/Aleatorizacion/Ejercicio22.java
@@ -9,3 +9,7 @@ import java.util.Scanner;
 public class Ejercicio22 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
+    System.out.println("            @");
+    
+  }
+}
