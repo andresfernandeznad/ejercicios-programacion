981e96d11eebd56757ff21e038884fff05ebda45
andresfernandeznad <andresfernandeznadales@gmail.com>
2017-11-22 11:17:59 +0100

    Añadido nuevo ejercicio

--- Ejercicios/Array/Ejercicio10.java
+++ Ejercicios/Array/Ejercicio10.java
@@ -15,20 +15,26 @@ public class Ejercicio10 {
     int [] numero = new int [20];
     int [] auxiliar = new int [20];
     int par = 0;
+    int impar = 0;
     for (int i = 0; i < 20; i++) {
        numero[i] = (int)((Math.random())*100) + 1;
+       
        if ((numero[i]%2)==0) {
         auxiliar[par] = numero[i];
         par++;
-      
+       } else {
+          numero[impar] = numero [i];
+          ++impar;
        }
+       
     }
    for (int i = 0; i < par; ++i) {
      System.out.print(auxiliar[i] + " ");
    }
-   for (int i = par; i < 20; i++) {
+   for (int i = 0; i < impar; ++i) {
      System.out.print(numero[i] + " ");
    }
+   
     
    
   }
