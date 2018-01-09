414d92106a55bc93274727a734832c2d8ae885d2
andresfernandeznad <andresfernandeznadales@gmail.com>
2018-01-09 09:41:32 +0100

    Ejercicio ampliado

--- Ejercicios/Funciones/Funciones.java
+++ Ejercicios/Funciones/Funciones.java
@@ -30,6 +30,29 @@ public class Funciones {
       System.out.println("Su número no es primo.");
     }
     
+    System.out.println("Su siguiente primo será: " + matematicas.Matematicas.siguientePrimo(n));
+    
+    System.out.println("Su número tiene " + matematicas.Matematicas.digitos(n) + " dígitos.");
+    
+    System.out.print("Introduce la base: ");
+    int base = s.nextInt();
+    System.out.print("Introduce el exponente: ");
+    int exponente = s.nextInt();
+    System.out.println("Su potencia será: " + matematicas.Matematicas.potencia(base, exponente));
+    
+    System.out.print("Introduce un número: ");
+    int numero = s.nextInt();
+    int copia = numero;
+    System.out.print("Introduce la posición del dígito: ");
+    int posicion = s.nextInt();
+    System.out.println("Su dígito es: " + matematicas.Matematicas.digitoN(numero, posicion));
+    
+    System.out.print("Introduce los dígitos que le quieres quitar: ");
+    int quitar = s.nextInt();
+    System.out.println("Su nuevo número es: " + matematicas.Matematicas.quitaPorDetras(numero, quitar));
+    System.out.println("Su nuevo número es: " + matematicas.Matematicas.quitaPorDelante(copia, quitar));
+    
+    
   }
   
 }
--- Ejercicios/Funciones/matematicas/Matematicas.java
+++ Ejercicios/Funciones/matematicas/Matematicas.java
@@ -43,4 +43,67 @@ public class Matematicas {
     }
     return true;
   }
+  
+  public static int siguientePrimo (int x) {
+    boolean esPrimo = false;
+    while (esPrimo == false) {
+      int j = 2;
+      while ((x % j) != 0) {
+        ++j;
+      }
+      if (j == x) {
+        
+        esPrimo = true;
+      }
+      ++x;
+    }
+    return (x - 1);
+  }
+  
+  public static int potencia (int x, int y) {
+    int potencia;
+    if (y == 0) {
+      potencia = 1;
+    } else {  
+      potencia = x;
+      for (int i = 0; i < y - 1; i++) {
+        potencia *= x;
+      }
+    }
+    return potencia;
+    
+  }
+  
+  public static int digitos (int x) {
+    int contador = 0;
+    while (x > 0) {
+      x /= 10;
+      ++contador;
+    }
+    return contador;
+  }
+  
+  public static int digitoN (int x, int y) {
+    int volteado = matematicas.Matematicas.voltea(x);
+    int solu = volteado % 10;
+    
+    for (int i = 0; i < y; i++) {
+      volteado /= 10;
+      solu = volteado % 10;
+    }
+    return solu;
+  }
+  
+  public static int quitaPorDetras (int x, int y) {
+    for (int i = 0; i < y; i++) {
+      x /= 10;
+    }
+    return x;
+  }
+  public static int quitaPorDelante (int x, int y) {
+    int volteado = matematicas.Matematicas.voltea(x);
+    x = matematicas.Matematicas.quitaPorDetras(volteado, y);
+    x = matematicas.Matematicas.voltea(x);
+    return x;
+  }
 }
